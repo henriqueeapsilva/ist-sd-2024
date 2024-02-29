5339925ad06d805b70da8045d750c62e22262700
Henrique Silva <henriqueapsilva@tecnico.ulisboa.pt>
2024-02-29 13:20:48 +0000

    feat: ServerState and ServiceImpl implemented

--- NameServer/NamingServerServiceImpl.py
+++ NameServer/NamingServerServiceImpl.py
@@ -1,4 +1,12 @@
-class HelloWorldServiceImpl(pb2_grpc.HelloWorldServiceServicer):
+import sys
+sys.path.insert(0, '../Contract/target/generated-sources/protobuf/python')
+import NameServer_pb2 as pb2
+import NameServer_pb2_grpc as pb2_grpc
+class NamingServerServiceImpl(pb2_grpc.NameServerServicer):
 
     def __init__(self, *args, **kwargs):
         pass
+
+    def register(self, request, context):
+        name
+
--- NameServer/domain/ServerState.py
+++ NameServer/domain/ServerState.py
@@ -1,2 +1,18 @@
+class ServerEntry:
+
+    def __init__(self, address, qualifier):
+        self.address = address
+        self.qualifier = qualifier
 
 
+class ServiceEntry:
+
+    def __init__(self, name, entries):
+        self.serviceName = name
+        self.serverEntries = entries
+
+
+class NamingServer:
+
+    def __init__(self, infomap):
+        self.map = infomap
