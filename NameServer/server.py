978773e85b2751f5b46da6977eb94f245e0f10d3
Tomás Macieira <tomasmacieira@tecnico.ulisboa.pt>
2024-02-29 09:52:22 +0000

    feat: added ServerState.py placeholder

--- NameServer/server.py
+++ NameServer/server.py
@@ -1,4 +1,6 @@
 import sys
+import grpc
+from concurrent import futures
 sys.path.insert(1, '../Contract/target/generated-sources/protobuf/python')
 
 # define the port
@@ -11,8 +13,13 @@ if __name__ == '__main__':
         for i in range(1, len(sys.argv)):
             print("  " + sys.argv[i])
 
-        # TODO
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+        server.add_insecure_port('[::]:'+ PORT))
+        server.start()
+        print('')
+        server.wait_for_termination()
 
     except KeyboardInterrupt:
         print("HelloServer stopped")
         exit(0)
+
