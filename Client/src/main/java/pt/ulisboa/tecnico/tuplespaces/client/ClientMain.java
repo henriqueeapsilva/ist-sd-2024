fb0da5577cd3c5c45a9be83d197cbda381b1662b
Henrique Silva <94613613+henriqueeapsilva@users.noreply.github.com>
2024-02-29 10:47:38 +0000

    fix: Update ClientMain.java

--- Client/src/main/java/pt/ulisboa/tecnico/tuplespaces/client/ClientMain.java
+++ Client/src/main/java/pt/ulisboa/tecnico/tuplespaces/client/ClientMain.java
@@ -5,13 +5,13 @@ import pt.ulisboa.tecnico.tuplespaces.client.grpc.ClientService;
 public class ClientMain {
     public static void main(String[] args) {
 
-        System.out.println(ClientMain.class.getSimpleName());
+        System.err.println(ClientMain.class.getSimpleName());
 
 
         // receive and print arguments
-        System.out.printf("Received %d arguments%n", args.length);
+        System.err.printf("Received %d arguments%n", args.length);
         for (int i = 0; i < args.length; i++) {
-            System.out.printf("arg[%d] = %s%n", i, args[i]);
+            System.err.printf("arg[%d] = %s%n", i, args[i]);
         }
 
         // check arguments
