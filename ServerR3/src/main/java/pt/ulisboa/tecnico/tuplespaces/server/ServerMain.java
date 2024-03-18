2dc43a3987894821e2971fd7f9358fc96dcb9f7b
Henrique Silva <henriqueapsilva@tecnico.ulisboa.pt>
2024-03-18 10:44:05 +0000

    Added Sequencer and ServerR3 folders to the project

---
+++ ServerR3/src/main/java/pt/ulisboa/tecnico/tuplespaces/server/ServerMain.java
@@ -0,0 +1,55 @@
+package pt.ulisboa.tecnico.tuplespaces.server;
+
+import io.grpc.*;
+import pt.ulisboa.tecnico.tuplespaces.centralized.contract.NameServerGrpc;
+import pt.ulisboa.tecnico.tuplespaces.centralized.contract.NameServerOuterClass;
+
+import java.io.IOException;
+
+public class ServerMain {
+
+    public static void main(String[] args) throws IOException, InterruptedException{
+
+        System.err.println(ServerMain.class.getSimpleName());
+
+        // receive and print arguments
+        System.err.printf("Received %d arguments%n", args.length);
+        for (int i = 0; i < args.length; i++) {
+            System.err.printf("arg[%d] = %s%n", i, args[i]);
+        }
+        // check arguments
+        if (args.length < 1) {
+            System.err.println("Argument(s) missing!");
+            System.err.printf("Usage: java %s port%n", ServerMain.class.getName());
+        }
+        final int port = Integer.parseInt(args[0]);
+        final BindableService impl = new ServerServiceImp();
+        final String qualifier = args[1];
+        final String service = "TupleSpaces";
+
+        // Create a new server to listen on port
+        Server server = ServerBuilder.forPort(port).addService(impl).build();
+
+        // Register on NamingServer
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 5001).usePlaintext().build();
+
+        String target = "localhost:" + port;
+
+        NameServerGrpc.NameServerBlockingStub stub = NameServerGrpc.newBlockingStub(channel);
+        NameServerOuterClass.registerResponse response = stub.register(NameServerOuterClass.registerRequest.newBuilder()
+                .setName(service).setQualifier(qualifier).setAddress(target).build());
+
+        server.start();
+        System.err.println("Server started");
+
+        // Handle Ctrl + C
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            // Delete server from NameServer
+            stub.delete(NameServerOuterClass.deleteRequest.newBuilder()
+                    .setServicename(service).setAddress(target).build());
+            server.shutdownNow();
+        }));
+        server.awaitTermination();
+    }
+}
+
---
+++ ServerR3/src/main/java/pt/ulisboa/tecnico/tuplespaces/server/ServerServiceImp.java
@@ -0,0 +1,105 @@
+package pt.ulisboa.tecnico.tuplespaces.server;
+
+import io.grpc.Status;
+import io.grpc.stub.StreamObserver;
+import pt.ulisboa.tecnico.tuplespaces.replicaXuLiskov.contract.TupleSpacesReplicaGrpc.TupleSpacesReplicaImplBase;
+import pt.ulisboa.tecnico.tuplespaces.server.domain.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static pt.ulisboa.tecnico.tuplespaces.replicaXuLiskov.contract.TupleSpacesReplicaXuLiskov.*;
+
+public class
+ServerServiceImp extends TupleSpacesReplicaImplBase {
+    private final ServerState tuplespaces = new ServerState();
+
+    @Override
+    public void put(PutRequest request, StreamObserver<PutResponse> responseObserver) {
+        try {
+            String newTuple = request.getNewTuple();
+
+            tuplespaces.put(newTuple);
+
+            // If the Response as no args, it serves as a placeholder to maintain the consistency of our service API.
+            PutResponse response = PutResponse.newBuilder().build();
+
+            // Send a single response through the stream.
+            responseObserver.onNext(response);
+            // Notify the client that the operation has been completed.
+            responseObserver.onCompleted();
+
+        } catch (IllegalArgumentException e) {
+            responseObserver.onError(Status.INVALID_ARGUMENT.withDescription("Invalid tuple format!").asRuntimeException());
+        }
+    }
+
+    @Override
+    public void read(ReadRequest request, StreamObserver<ReadResponse> responseObserver) {
+        try {
+            String searchPattern = request.getSearchPattern();
+
+            ReadResponse response = ReadResponse.newBuilder().setResult(tuplespaces.read(searchPattern)).build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+
+        } catch (IllegalArgumentException e) {
+            responseObserver.onError(Status.INVALID_ARGUMENT.withDescription("Invalid tuple format!").asRuntimeException());
+        }
+    }
+
+    @Override
+    public void takePhase1(TakePhase1Request request, StreamObserver<TakePhase1Response> responseObserver) {
+        try {
+            Integer clientId = request.getClientId();
+            String searchPattern = request.getSearchPattern();
+            TakePhase1Response response;
+
+            // verifies if there is any match -> if not waits
+            tuplespaces.waitForMatchingTuple(searchPattern,false);
+            List<String> tuples = tuplespaces.getAllMatchingTuples(searchPattern, clientId);
+
+            response = TakePhase1Response.newBuilder()
+                        .addAllReservedTuples(tuples).build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+
+        } catch (IllegalArgumentException e) {
+            responseObserver.onError(Status.INVALID_ARGUMENT.withDescription("Invalid tuple").asRuntimeException());
+        }
+    }
+
+    @Override
+    public void takePhase1Release(TakePhase1ReleaseRequest request, StreamObserver<TakePhase1ReleaseResponse> responseObserver) {
+        int clientId = request.getClientId();
+        tuplespaces.releaseLocks(clientId);
+
+        TakePhase1ReleaseResponse releaseResponse = TakePhase1ReleaseResponse.newBuilder().build();
+        responseObserver.onNext(releaseResponse);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void takePhase2(TakePhase2Request request, StreamObserver<TakePhase2Response> responseObserver) {
+        int clientId = request.getClientId();
+        String pattern = request.getTuple();
+
+        tuplespaces.releaseLocks(clientId);
+        tuplespaces.take(pattern);
+
+        TakePhase2Response response = TakePhase2Response.newBuilder().build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getTupleSpacesState(getTupleSpacesStateRequest request, StreamObserver<getTupleSpacesStateResponse> responseObserver) {
+
+        getTupleSpacesStateResponse response = getTupleSpacesStateResponse.newBuilder().addAllTuple(tuplespaces.getTupleSpacesState()).build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
